4337d26ba5eed86e3b75753b0622e219ecf199a6
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-09-10 23:58:48 +0900

    Fix: spec of CHECK-BNF due to syntax is changed.
    Affect:	commit 21666ccf43ca144bb5e68ea311529fcf996441af

--- spec/check-bnf.lisp
+++ spec/check-bnf.lisp
@@ -11,189 +11,193 @@
 ; In most simple case, its like CL:CHECK-TYPE.
 #?(let((var 'symbol))
     (check-bnf()
-      (var symbol)))
+      ((var symbol))))
 => NIL
 #?(let((var "string"))
     (check-bnf()
-      (var symbol)))
+      ((var symbol))))
 :signals syntax-error
 
 ; You can check some place at once.
 #?(let((a 'symbol)
        (b "string"))
     (check-bnf()
-      (a symbol)
-      (b string)))
+      ((a symbol))
+      ((b string))))
 => NIL
 
 ; When type-specifier is expanded to T,
 ; Efficient code is generated.
 #?(check-bnf()
-    (a t))
+    ((a t)))
 :expanded-to
-(labels((a(a)
-	  (declare(ignore a))
-	  nil))
-  (let((check-bnf::*whole* nil)
-       (check-bnf::*bnf* '((a t))))))
+(let((check-bnf::*whole* nil))
+  (let((check-bnf::*bnf* '((a t))))
+    (labels((a(a)
+	      (declare(ignore a))
+	      nil))
+      (a a))))
 
 #?(check-bnf()
-    (a* t))
+    ((a* t)))
 :expanded-to
-(labels((a*(a*)
-	  (if (typep a* '(and atom (not null)))
-	    (syntax-error 'a* "Require LIST but ~S." a*)
-	    nil)))
-  (let((check-bnf::*whole* nil)
-       (check-bnf::*bnf* '((a* t))))))
+(let((check-bnf::*whole* nil))
+  (let((check-bnf::*bnf* '((a* t))))
+    (labels((a*(a*)
+	      (if (typep a* '(and atom (not null)))
+		(syntax-error 'a* "Require LIST but ~S." a*)
+		nil)))
+      (a* a*))))
 
 ; Also it occur in OR form.
 #?(check-bnf()
-    (a (or symbol t string)))
+    ((a (or symbol t string))))
 :expanded-to
-(labels((a(a)
-	  (declare(ignore a))
-	  nil))
-  (let((check-bnf::*whole* nil)
-       (check-bnf::*bnf* '((a (or symbol t string)))))))
+(let((check-bnf::*whole* nil))
+  (let((check-bnf::*bnf* '((a (or symbol t string)))))
+    (labels((a(a)
+	      (declare(ignore a))
+	      nil))
+      (a a))))
 
 #?(check-bnf()
-    (a (or symbol b string))
-    (b t))
+    ((a (or symbol b string))
+     (b t)))
 :expanded-to
-(labels((a(a)
-	  (declare(ignore a))
-	  nil)
-	(b(b)
-	  (declare(ignore b))
-	  nil))
-  (let((check-bnf::*whole* nil)
-       (check-bnf::*bnf* '((a (or symbol b string))
-			   (b t))))))
+(let((check-bnf::*whole* nil))
+  (let((check-bnf::*bnf* '((a (or symbol b string))
+			   (b t))))
+    (labels((a(a)
+	      (declare(ignore a))
+	      nil)
+	    (b(b)
+	      (declare(ignore b))
+	      nil))
+      (a a))))
 
 #?(check-bnf()
-    (a (or b c))
-    (b integer)
-    (c t))
+    ((a (or b c))
+     (b integer)
+     (c t)))
 :expanded-to
-(labels((a(a)
-	  (declare(ignore a))
-	  nil)
-	(b(b)
-	  (unless(typep b 'integer)
-	    (syntax-error 'b "but ~S, it is type-of ~S"
-			  b (type-of b))))
-	(c(c)
-	  (declare(ignore c))
-	  nil))
-  (let((check-bnf::*whole* nil)
-       (check-bnf::*bnf* '((a (or b c))
+(let((check-bnf::*whole* nil))
+  (let((check-bnf::*bnf* '((a (or b c))
 			   (b integer)
-			   (c t))))))
+			   (c t))))
+    (labels((a(a)
+	      (declare(ignore a))
+	      nil)
+	    (b(b)
+	      (unless(typep b 'integer)
+		(syntax-error 'b "but ~S, it is type-of ~S"
+			      b (type-of b))))
+	    (c(c)
+	      (declare(ignore c))
+	      nil))
+      (a a))))
 
 ; When you know VAR is list, and it has 0 or more elt. (a.k.a. *)
 ; You can write like below.
 #?(let((var* nil))
     (check-bnf()
-      (var* symbol)))
+      ((var* symbol))))
 => NIL
 
 #?(let((var* '(symbol)))
     (check-bnf()
-      (var* symbol)))
+      ((var* symbol))))
 => NIL
 
 #?(let((var* '("string")))
     (check-bnf()
-      (var* symbol)))
+      ((var* symbol))))
 :signals syntax-error
 
 #?(let((var* :not-list))
     (check-bnf()
-      (var* symbol)))
+      ((var* symbol))))
 :signals syntax-error
 
 ; When expected T, efficient code is generated.
 #?(let((option* ()))
     (check-bnf:check-bnf()
-      (option* keyword T)))
+      ((option* keyword T))))
 => NIL
 
 ; If you do not like names var as XXX*, you can specify alias.
 #?(let((vars '(symbol)))
-    (declare(ignore vars))
     (check-bnf()
-      ((var* vars)symbol)))
+      (((var* vars)symbol))))
 => NIL
 
 ; e.g. specify for plist.
 #?(let((var* '(:key "value" :key2 "value2")))
     (check-bnf()
-      (var* keyword string)))
+      ((var* keyword string))))
 => NIL
 
 #?(let((var* '(:key 2 :key2 "not integer")))
     (check-bnf()
-      (var* keyword integer)))
+      ((var* keyword integer))))
 :signals syntax-error
 
 #?(let((var* '(:key 1 "not-key" 2)))
     (check-bnf()
-      (var* keyword integer)))
+      ((var* keyword integer))))
 :signals syntax-error
 
 #?(let((var* '(not "ballanced" plist)))
     (check-bnf()
-      (var* keyword string)))
+      ((var* keyword string))))
 :signals syntax-error
 
 ; e.g. specify for alist.
 #?(let((var* '((:key "value")(:key2 "value2"))))
     (check-bnf()
-      (var* (keyword string))))
+      ((var* (keyword string)))))
 => NIL
 
 #?(let((var* '((:key "value")(:key2 :not-string))))
     (check-bnf()
-      (var* (keyword string))))
+      ((var* (keyword string)))))
 :signals syntax-error
 
 #?(let((var* '((:key "value")(:not "ballanced" clause))))
     (check-bnf()
-      (var* (keyword string))))
+      ((var* (keyword string)))))
 :signals syntax-error
 
 #?(let((var* '((:key "value")(:not-ballanced))))
     (check-bnf()
-      (var* (keyword string))))
+      ((var* (keyword string)))))
 :signals syntax-error
 
 ; of course dotted are valid.
 #?(let((var* '((:key . "value"))))
     (check-bnf()
-      (var* (keyword . string))))
+      ((var* (keyword . string)))))
 => NIL
 
 ; When you know VAR is list, and it has 1 or more elt, (a.k.a. +)
 ; you can write like below.
 #?(let((var+ '(1)))
     (check-bnf()
-      (var+ integer)))
+      ((var+ integer))))
 => NIL
 
 #?(let((var+ '()))
     (check-bnf()
-      (var+ integer)))
+      ((var+ integer))))
 :signals syntax-error
 
 #?(let((var+ '("not-integer")))
     (check-bnf()
-      (var+ integer)))
+      ((var+ integer))))
 :signals syntax-error
 
 #?(let((var+ :not-cons))
     (check-bnf()
-      (var+ integer)))
+      ((var+ integer))))
 :signals syntax-error
 
 #+syntax
@@ -203,7 +207,7 @@
 
 ; whole := form, evaluated.
 #?(check-bnf(:whole no-such-var)
-    (dummy dummy))
+    ((dummy dummy)))
 :signals (or error
 	     warning ; for ccl
 	     )
@@ -212,7 +216,7 @@
 ; When specified, header and footer is generated in error message.
 #?(let((a "not-symbol"))
     (check-bnf()
-      (a symbol)))
+      ((a symbol))))
 :invokes-debugger syntax-error
 ,:test (lambda(condition)
 	 (equal #.(format nil "A := SYMBOL~%~
@@ -222,7 +226,7 @@
 
 #?(let((a "not-symbol"))
     (check-bnf(:whole '(whole ("not-symbol")))
-      (a symbol)))
+      ((a symbol))))
 :invokes-debugger syntax-error
 ,:test (lambda(condition)
 	 (equal #.(format nil "Syntax-error in WHOLE~%~
@@ -237,13 +241,13 @@
 ; var-spec := [ name | (name name) ]
 ; name := SYMBOL, otherwise error.
 #?(check-bnf()
-    ("not-symbol" dummy))
+    (("not-symbol" dummy)))
 :signals syntax-error
 ,:lazy t
 
 ; Not evaluated.
 #?(check-bnf()
-    ((intern "not evaluated") dummy))
+    (((intern "not evaluated") dummy)))
 :signals syntax-error
 ,:lazy t
 
@@ -259,12 +263,12 @@
 
 ; Spec require one or more, otherwise error.
 #?(check-bnf()
-    (dummy))
+    ((dummy)))
 :signals syntax-error
 ,:lazy t
 
 #?(check-bnf()
-    (dummy+ nil))
+    ((dummy+ nil)))
 :signals syntax-error
 ,:lazy t
 
@@ -280,128 +284,128 @@
 ; To check optional argument (e.g. &OPTIONAL or &KEY), you can write like below.
 #?(let((option))
     (check-bnf()
-      (option (or null symbol))))
+      ((option (or null symbol)))))
 => NIL
 
 #?(let((option 'symbol))
     (check-bnf()
-      (option (or null symbol))))
+      ((option (or null symbol)))))
 => NIL
 
 #?(let((option "not-symbol"))
     (check-bnf()
-      (option (or null symbol))))
+      ((option (or null symbol)))))
 :signals syntax-error
 
 #?(let((function-name "not function-name"))
     (check-bnf()
-      (function-name (or name setf-name))
-      (name symbol)
-      (setf-name ((eql setf)name))))
+      ((function-name (or name setf-name))
+       (name symbol)
+       (setf-name ((eql setf)name)))))
 :signals syntax-error
 
 ; To check optional value in list, you can write like below.
 #?(let((args '(option and others)))
     (check-bnf()
-      (args (option? other*))
-      (option? (eql option))
-      (other* symbol)))
+      ((args (option? other*))
+       (option? (eql option))
+       (other* symbol))))
 => NIL
 
 #?(let((args '(and others)))
     (check-bnf()
-      (args (option? other*))
-      (option? (eql option))
-      (other* symbol)))
+      ((args (option? other*))
+       (option? (eql option))
+       (other* symbol))))
 => NIL
 
 #?(let((args '("not option nor other*" and others)))
     (check-bnf()
-      (args (option? other*))
-      (option? (eql option))
-      (other* symbol)))
+      ((args (option? other*))
+       (option? (eql option))
+       (other* symbol))))
 :signals syntax-error
 
 ;;;; Exceptional-Situations:
 ; Every NAME should not conflicts CL symbol.
 #?(let((list nil))
     (check-bnf()
-      (list list)))
+      ((list list))))
 => unspecified
 
 ;;;; Example.
 #?(let((var '(symbol symbol)))
     (check-bnf()
-      (var (symbol symbol option*))
-      (option* keyword string)))
+      ((var (symbol symbol option*))
+       (option* keyword string))))
 => NIL
 
 #?(let((var '(symbol symbol)))
     (check-bnf()
-      (var (symbol symbol option?))
-      (option? keyword)))
+      ((var (symbol symbol option?))
+       (option? keyword))))
 => NIL
 
 #?(let((var '(symbol symbol)))
     (check-bnf()
-      (var (symbol symbol required))
-      (required keyword)))
+      ((var (symbol symbol required))
+       (required keyword))))
 :signals syntax-error
 
 ;; Right side XXX*
 #?(let((var '(symbol)))
     (check-bnf()
-      (var (or string name*))
-      (name symbol)))
+      ((var (or string name*))
+       (name symbol))))
 => NIL
 
 #?(let((var "string"))
     (check-bnf()
-      (var (or string name*))
-      (name symbol)))
+      ((var (or string name*))
+       (name symbol))))
 => NIL
 
 #?(let((var ()))
     (check-bnf()
-      (var (or string name*))
-      (name symbol)))
+      ((var (or string name*))
+       (name symbol))))
 => NIL
 
 #?(let((var :not-list))
     (check-bnf()
-      (var (or string name*))
-      (name symbol)))
+      ((var (or string name*))
+       (name symbol))))
 :signals syntax-error
 
 #?(let((var '("string" "list")))
     (check-bnf()
-      (var (or string name*))
-      (name symbol)))
+      ((var (or string name*))
+       (name symbol))))
 :signals syntax-error
 
 ;; Right side XXX?
 #?(let((ll nil))
     (check-bnf()
-      (ll (var?))
-      (var symbol)))
+      ((ll (var?))
+       (var symbol))))
 => NIL
 
 #?(let((ll '(var)))
     (check-bnf()
-      (ll (var?))
-      (var symbol)))
+      ((ll (var?))
+       (var symbol))))
 => NIL
 
 #?(let((ll '(var too much)))
     (check-bnf()
-      (ll (var?))
-      (var symbol)))
+      ((ll (var?))
+       (var symbol))))
 :signals syntax-error
 
 #?(let((ll "not symbol"))
     (check-bnf()
-      (ll (var?))
-      (var symbol)))
+      ((ll (var?))
+       (var symbol))))
 :signals syntax-error
 
 (requirements-about EXPRESSION :doc-type type)
--- spec/check-bnf.test.asd
+++ spec/check-bnf.test.asd
@@ -2,14 +2,11 @@
 (in-package :asdf)
 (defsystem "check-bnf.test"
   :version
-  "0.2.16"
+  "0.2.17"
   :depends-on
   (:jingoh "check-bnf")
   :components
   ((:file "check-bnf"))
   :perform
   (test-op (o c) (declare (special args))
-    (if(fboundp(find-symbol "VARIABLE-INFORMATION" "CLTL2"))
-      (apply #'symbol-call :jingoh :examine :check-bnf args)
-      (warn "CHECK-BNF does not work in ~A, due to TRIVIAL-CLTL2 does not support."
-            (lisp-implementation-type)))))
+    (apply #'symbol-call :jingoh :examine :check-bnf args)))
