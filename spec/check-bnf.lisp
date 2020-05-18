396818bb54e76ca0daad43da3673bb25dc568181
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-05-18 23:32:39 +0900

    Cleanup: Spec format.

--- spec/check-bnf.lisp
+++ spec/check-bnf.lisp
@@ -10,267 +10,267 @@
 ; Macro argument checker like CL:CHECK-TYPE.
 
 ; In most simple case, its like CL:CHECK-TYPE.
-#?(let((var 'symbol))
-    (check-bnf()
+#?(let ((var 'symbol))
+    (check-bnf ()
       ((var symbol))))
 => NIL
-#?(let((var "string"))
-    (check-bnf()
+#?(let ((var "string"))
+    (check-bnf ()
       ((var symbol))))
 :signals syntax-error
 
 ; You can check some place at once.
-#?(let((a 'symbol)
-       (b "string"))
-    (check-bnf()
+#?(let ((a 'symbol)
+        (b "string"))
+    (check-bnf ()
       ((a symbol))
       ((b string))))
 => NIL
 
 ; When type-specifier is expanded to T,
 ; Efficient code is generated.
-#?(check-bnf()
+#?(check-bnf ()
     ((a t)))
 :expanded-to
-(let((check-bnf::*whole* nil))
-  (let((check-bnf::*bnf* '((a t))))
-    (labels((a(a)
-	      (declare(ignore a))
-	      nil))
+(let ((check-bnf::*whole* nil))
+  (let ((check-bnf::*bnf* '((a t))))
+    (labels ((a (a)
+               (declare (ignore a))
+               nil))
       (a a))))
 
-#?(check-bnf()
+#?(check-bnf ()
     ((a* t)))
 :expanded-to
-(let((check-bnf::*whole* nil))
-  (let((check-bnf::*bnf* '((a* t))))
-    (labels((a*(a*)
-	      (if (typep a* '(and atom (not null)))
-		(syntax-error 'a* "Require LIST but ~S." a*)
-		nil)))
+(let ((check-bnf::*whole* nil))
+  (let ((check-bnf::*bnf* '((a* t))))
+    (labels ((a* (a*)
+              (if (typep a* '(and atom (not null)))
+                  (syntax-error 'a* "Require LIST but ~S." a*)
+                  nil)))
       (a* a*))))
 
 ; Also it occur in OR form.
-#?(check-bnf()
+#?(check-bnf ()
     ((a (or symbol t string))))
 :expanded-to
-(let((check-bnf::*whole* nil))
-  (let((check-bnf::*bnf* '((a (or symbol t string)))))
-    (labels((a(a)
-	      (declare(ignore a))
-	      nil))
+(let ((check-bnf::*whole* nil))
+  (let ((check-bnf::*bnf* '((a (or symbol t string)))))
+    (labels ((a (a)
+               (declare (ignore a))
+               nil))
       (a a))))
 
-#?(check-bnf()
+#?(check-bnf ()
     ((a (or symbol b string))
      (b t)))
 :expanded-to
-(let((check-bnf::*whole* nil))
-  (let((check-bnf::*bnf* '((a (or symbol b string))
-			   (b t))))
-    (labels((a(a)
-	      (declare(ignore a))
-	      nil)
-	    (b(b)
-	      (declare(ignore b))
-	      nil))
+(let ((check-bnf::*whole* nil))
+  (let ((check-bnf::*bnf* '((a (or symbol b string))
+                             (b t))))
+    (labels ((a (a)
+               (declare (ignore a))
+               nil)
+             (b (b)
+               (declare (ignore b))
+               nil))
       (a a))))
 
-#?(check-bnf()
+#?(check-bnf ()
     ((a (or b c))
      (b integer)
      (c t)))
 :expanded-to
-(let((check-bnf::*whole* nil))
-  (let((check-bnf::*bnf* '((a (or b c))
-			   (b integer)
-			   (c t))))
-    (labels((a(a)
-	      (declare(ignore a))
-	      nil)
-	    (b(b)
-	      (unless(typep b 'integer)
-		(syntax-error 'b "but ~S, it is type-of ~S"
-			      b (type-of b))))
-	    (c(c)
-	      (declare(ignore c))
-	      nil))
+(let ((check-bnf::*whole* nil))
+  (let ((check-bnf::*bnf* '((a (or b c))
+                             (b integer)
+                             (c t))))
+    (labels ((a (a)
+               (declare (ignore a))
+               nil)
+             (b (b)
+               (unless (typep b 'integer)
+                 (syntax-error 'b "but ~S, it is type-of ~S"
+                                b (type-of b))))
+             (c (c)
+               (declare (ignore c))
+               nil))
       (a a))))
 
 ; When you know VAR is list, and it has 0 or more elt. (a.k.a. *)
 ; You can write like below.
-#?(let((var* nil))
-    (check-bnf()
+#?(let ((var* nil))
+    (check-bnf ()
       ((var* symbol))))
 => NIL
 
-#?(let((var* '(symbol)))
-    (check-bnf()
+#?(let ((var* '(symbol)))
+    (check-bnf ()
       ((var* symbol))))
 => NIL
 
-#?(let((var* '("string")))
-    (check-bnf()
+#?(let ((var* '("string")))
+    (check-bnf ()
       ((var* symbol))))
 :signals syntax-error
 
-#?(let((var* :not-list))
-    (check-bnf()
+#?(let ((var* :not-list))
+    (check-bnf ()
       ((var* symbol))))
 :signals syntax-error
 
 ; When expected T, efficient code is generated.
-#?(let((option* ()))
-    (check-bnf:check-bnf()
+#?(let ((option* ()))
+    (check-bnf:check-bnf ()
       ((option* keyword T))))
 => NIL
 
 ; If you do not like names var as XXX*, you can specify alias.
-#?(let((vars '(symbol)))
-    (check-bnf()
-      (((var* vars)symbol))))
+#?(let ((vars '(symbol)))
+    (check-bnf ()
+      (((var* vars) symbol))))
 => NIL
 
 ; e.g. specify for plist.
-#?(let((var* '(:key "value" :key2 "value2")))
-    (check-bnf()
+#?(let ((var* '(:key "value" :key2 "value2")))
+    (check-bnf ()
       ((var* keyword string))))
 => NIL
 
-#?(let((var* '(:key 2 :key2 "not integer")))
-    (check-bnf()
+#?(let ((var* '(:key 2 :key2 "not integer")))
+    (check-bnf ()
       ((var* keyword integer))))
 :signals syntax-error
 
-#?(let((var* '(:key 1 "not-key" 2)))
-    (check-bnf()
+#?(let ((var* '(:key 1 "not-key" 2)))
+    (check-bnf ()
       ((var* keyword integer))))
 :signals syntax-error
 
-#?(let((var* '(not "ballanced" plist)))
-    (check-bnf()
+#?(let ((var* '(not "ballanced" plist)))
+    (check-bnf ()
       ((var* keyword string))))
 :signals syntax-error
 
 ; e.g. specify for alist.
-#?(let((var* '((:key "value")(:key2 "value2"))))
-    (check-bnf()
+#?(let ((var* '((:key "value") (:key2 "value2"))))
+    (check-bnf ()
       ((var* (keyword string)))))
-=> NIL
+=> nil
 
-#?(let((var* '((:key "value")(:key2 :not-string))))
-    (check-bnf()
-      ((var* (keyword string)))))
-:signals syntax-error
-
-#?(let((var* '((:key "value")(:not "ballanced" clause))))
-    (check-bnf()
+#?(let ((var* '((:key "value") (:key2 :not-string))))
+    (check-bnf ()
       ((var* (keyword string)))))
 :signals syntax-error
 
-#?(let((var* '((:key "value")(:not-ballanced))))
-    (check-bnf()
+#?(let ((var* '((:key "value") (:not "ballanced" clause))))
+    (check-bnf ()
+      ((var* (keyword string)))))
+:signals syntax-error
+
+#?(let ((var* '((:key "value") (:not-ballanced))))
+    (check-bnf ()
       ((var* (keyword string)))))
 :signals syntax-error
 
 ; of course dotted are valid.
-#?(let((var* '((:key . "value"))))
-    (check-bnf()
+#?(let ((var* '((:key . "value"))))
+    (check-bnf ()
       ((var* (keyword . string)))))
-=> NIL
+=> nil
 
-; When you know VAR is list, and it has 1 or more elt, (a.k.a. +)
+; when you know var is list, and it has 1 or more elt, (a.k.a. +)
 ; you can write like below.
-#?(let((var+ '(1)))
-    (check-bnf()
+#?(let ((var+ '(1)))
+    (check-bnf ()
       ((var+ integer))))
-=> NIL
+=> nil
 
-#?(let((var+ '()))
-    (check-bnf()
-      ((var+ integer))))
-:signals syntax-error
-
-#?(let((var+ '("not-integer")))
-    (check-bnf()
+#?(let ((var+ '()))
+    (check-bnf ()
       ((var+ integer))))
 :signals syntax-error
 
-#?(let((var+ :not-cons))
-    (check-bnf()
+#?(let ((var+ '("not-integer")))
+    (check-bnf ()
+      ((var+ integer))))
+:signals syntax-error
+
+#?(let ((var+ :not-cons))
+    (check-bnf ()
       ((var+ integer))))
 :signals syntax-error
 
 #+syntax
-(CHECK-BNF (&key ((:whole whole?))) &rest def+) ; => result
+(check-bnf (&key ((:whole whole?))) &rest def+) ; => result
 
-;;;; Arguments and Values:
+;;;; arguments and values:
 
 ; whole := form, evaluated.
-#?(check-bnf(:whole no-such-var)
+#?(check-bnf (:whole no-such-var)
     ((dummy dummy)))
 :signals (or error
-	     warning ; for ccl
-	     )
+             warning ; for ccl
+             )
 
-; Expects var for &WHOLE.
-; When specified, header and footer is generated in error message.
+; expects var for &whole.
+; when specified, header and footer is generated in error message.
 #-clisp
-#?(let((a "not-symbol"))
-    (check-bnf()
+#?(let ((a "not-symbol"))
+    (check-bnf ()
       ((a symbol))))
 :invokes-debugger syntax-error
-,:test (lambda(condition)
-	 (& (equal #.(format nil "A := SYMBOL~2%~
-			     but \"not-symbol\", it is type-of ~S"
-			     (type-of "not-symbol"))
-		   (princ-to-string condition))))
+,:test (lambda (condition)
+         (& (equal #.(format nil "a := symbol~2%~
+                             but \"not-symbol\", it is type-of ~s"
+                             (type-of "not-symbol"))
+                   (princ-to-string condition))))
 
 #-clisp
-#?(let((a "not-symbol"))
-    (check-bnf(:whole '(whole ("not-symbol")))
+#?(let ((a "not-symbol"))
+    (check-bnf (:whole '(whole ("not-symbol")))
       ((a symbol))))
 :invokes-debugger syntax-error
-,:test (lambda(condition)
-	 (& (equal #.(format nil "Syntax-error in WHOLE~2%~
-			     A := SYMBOL~2%~
-			     but \"not-symbol\", it is type-of ~S~2%~
-			     in ~S"
-			     (type-of "not-symbol")
-			     '(whole ("not-symbol")))
-		   (princ-to-string condition))))
+,:test (lambda (condition)
+         (& (equal #.(format nil "syntax-error in whole~2%~
+                             a := symbol~2%~
+                             but \"not-symbol\", it is type-of ~s~2%~
+                             in ~s"
+                             (type-of "not-symbol")
+                             '(whole ("not-symbol")))
+                   (princ-to-string condition))))
 
-; NOTE! CLISP specific bug(?).
-; I do not know why but CLISP does not call specified report function.
-; So header and footer is not generated.
-; After bug is removed, guard below will be failed.
+; note! clisp specific bug(?).
+; i do not know why but clisp does not call specified report function.
+; so header and footer is not generated.
+; after bug is removed, guard below will be failed.
 #+clisp
-#?(let((a "not-symbol"))
-    (check-bnf()
+#?(let ((a "not-symbol"))
+    (check-bnf ()
       ((a symbol))))
 :invokes-debugger syntax-error
-,:test (lambda(condition)
-	 (& (equal #.(format nil "but \"not-symbol\", it is type-of ~S"
-			     (type-of "not-symbol"))
-		   (princ-to-string condition))))
+,:test (lambda (condition)
+         (& (equal #.(format nil "but \"not-symbol\", it is type-of ~s"
+                             (type-of "not-symbol"))
+                   (princ-to-string condition))))
 
 ; def := (clause+)+
 ; clause := (var-spec spec+)
 ; var-spec := [ name | (name name) ]
-; name := SYMBOL, otherwise error.
-#?(check-bnf()
+; name := symbol, otherwise error.
+#?(check-bnf ()
     (("not-symbol" dummy)))
 :signals syntax-error
 ,:lazy t
 
-; Not evaluated.
-#?(check-bnf()
+; not evaluated.
+#?(check-bnf ()
     (((intern "not evaluated") dummy)))
 :signals syntax-error
 ,:lazy t
 
-; Clause require one or more, otherwise syntax error.
-#?(check-bnf())
+; clause require one or more, otherwise syntax error.
+#?(check-bnf ())
 :signals syntax-error
 ,:lazy t
 
@@ -279,173 +279,173 @@
 ; list-bnf := (spec+)
 ; or-form := (or spec+)
 
-; Spec require one or more, otherwise error.
-#?(check-bnf()
+; spec require one or more, otherwise error.
+#?(check-bnf ()
     ((dummy)))
 :signals syntax-error
 ,:lazy t
 
-#?(check-bnf()
+#?(check-bnf ()
     ((dummy+ nil)))
 :signals syntax-error
 ,:lazy t
 
-; result := NULL
+; result := null
 
-;;;; Affected By:
+;;;; affected by:
 ; none
 
-;;;; Side-Effects:
+;;;; side-effects:
 ; none
 
-;;;; Notes:
-; To check optional argument (e.g. &OPTIONAL or &KEY), you can write like below.
-#?(let((option))
-    (check-bnf()
+;;;; notes:
+; to check optional argument (e.g. &optional or &key), you can write like below.
+#?(let ((option))
+    (check-bnf ()
       ((option (or null symbol)))))
-=> NIL
+=> nil
 
-#?(let((option 'symbol))
-    (check-bnf()
+#?(let ((option 'symbol))
+    (check-bnf ()
       ((option (or null symbol)))))
-=> NIL
+=> nil
 
-#?(let((option "not-symbol"))
-    (check-bnf()
+#?(let ((option "not-symbol"))
+    (check-bnf ()
       ((option (or null symbol)))))
 :signals syntax-error
 
-#?(let((function-name "not function-name"))
-    (check-bnf()
+#?(let ((function-name "not function-name"))
+    (check-bnf ()
       ((function-name (or name setf-name))
        (name symbol)
-       (setf-name ((eql setf)name)))))
+       (setf-name ((eql setf) name)))))
 :signals syntax-error
 
-; To check optional value in list, you can write like below.
-#?(let((args '(option and others)))
-    (check-bnf()
+; to check optional value in list, you can write like below.
+#?(let ((args '(option and others)))
+    (check-bnf ()
       ((args (option? other*))
        (option? (eql option))
        (other* symbol))))
-=> NIL
+=> nil
 
-#?(let((args '(and others)))
-    (check-bnf()
+#?(let ((args '(and others)))
+    (check-bnf ()
       ((args (option? other*))
        (option? (eql option))
        (other* symbol))))
-=> NIL
+=> nil
 
-#?(let((args '("not option nor other*" and others)))
-    (check-bnf()
+#?(let ((args '("not option nor other*" and others)))
+    (check-bnf ()
       ((args (option? other*))
        (option? (eql option))
        (other* symbol))))
 :signals syntax-error
 
-;;;; Exceptional-Situations:
-; Every NAME should not conflicts CL symbol.
-#?(let((list nil))
-    (check-bnf()
+;;;; exceptional-situations:
+; every name should not conflicts cl symbol.
+#?(let ((list nil))
+    (check-bnf ()
       ((list list))))
 => unspecified
 
-;;;; Example.
-#?(let((var '(symbol symbol)))
-    (check-bnf()
+;;;; example.
+#?(let ((var '(symbol symbol)))
+    (check-bnf ()
       ((var (symbol symbol option*))
        (option* keyword string))))
-=> NIL
+=> nil
 
-#?(let((var '(symbol symbol)))
-    (check-bnf()
+#?(let ((var '(symbol symbol)))
+    (check-bnf ()
       ((var (symbol symbol option?))
        (option? keyword))))
-=> NIL
+=> nil
 
-#?(let((var '(symbol symbol)))
-    (check-bnf()
+#?(let ((var '(symbol symbol)))
+    (check-bnf ()
       ((var (symbol symbol required))
        (required keyword))))
 :signals syntax-error
 
-;; Right side XXX*
-#?(let((var '(symbol)))
-    (check-bnf()
+;; right side xxx*
+#?(let ((var '(symbol)))
+    (check-bnf ()
       ((var (or string name*))
        (name symbol))))
-=> NIL
+=> nil
 
-#?(let((var "string"))
-    (check-bnf()
+#?(let ((var "string"))
+    (check-bnf ()
       ((var (or string name*))
        (name symbol))))
-=> NIL
+=> nil
 
-#?(let((var ()))
-    (check-bnf()
+#?(let ((var ()))
+    (check-bnf ()
       ((var (or string name*))
        (name symbol))))
-=> NIL
+=> nil
 
-#?(let((var :not-list))
-    (check-bnf()
-      ((var (or string name*))
-       (name symbol))))
-:signals syntax-error
-
-#?(let((var '("string" "list")))
-    (check-bnf()
+#?(let ((var :not-list))
+    (check-bnf ()
       ((var (or string name*))
        (name symbol))))
 :signals syntax-error
 
-;; Right side XXX?
-#?(let((ll nil))
-    (check-bnf()
+#?(let ((var '("string" "list")))
+    (check-bnf ()
+      ((var (or string name*))
+       (name symbol))))
+:signals syntax-error
+
+;; right side xxx?
+#?(let ((ll nil))
+    (check-bnf ()
       ((ll (var?))
        (var symbol))))
-=> NIL
+=> nil
 
-#?(let((ll '(var)))
-    (check-bnf()
+#?(let ((ll '(var)))
+    (check-bnf ()
       ((ll (var?))
        (var symbol))))
-=> NIL
+=> nil
 
-#?(let((ll '(var too much)))
-    (check-bnf()
+#?(let ((ll '(var too much)))
+    (check-bnf ()
       ((ll (var?))
        (var symbol))))
 :signals syntax-error
 
-#?(let((ll "not symbol"))
-    (check-bnf()
+#?(let ((ll "not symbol"))
+    (check-bnf ()
       ((ll (var?))
        (var symbol))))
 :signals syntax-error
 
-(requirements-about EXPRESSION :doc-type type)
-;;;; Description:
-;;;; Compound Type Specifier Kind:
+(requirements-about expression :doc-type type)
+;;;; description:
+;;;; compound type specifier kind:
 
-;;;; Compound Type Specifier Syntax:
+;;;; compound type specifier syntax:
 
-;;;; Compound Type Specifier Arguments:
+;;;; compound type specifier arguments:
 
-;;;; Compound Type Specifier Description:
+;;;; compound type specifier description:
 
-(requirements-about PPRINT-CHECK-BNF :doc-type function
-                    :around (let((*print-pretty* t))
+(requirements-about pprint-check-bnf :doc-type function
+                    :around (let ((*print-pretty* t))
                               (call-body)))
 
-;;;; Description:
+;;;; description:
 
 #+syntax
-(PPRINT-CHECK-BNF stream exp) ; => result
+(pprint-check-bnf stream exp) ; => result
 
-;;;; Arguments and Values:
+;;;; arguments and values:
 
 ; stream := 
 
@@ -453,48 +453,48 @@
 
 ; result := 
 
-;;;; Affected By:
+;;;; affected by:
 
-;;;; Side-Effects:
+;;;; side-effects:
 
-;;;; Notes:
+;;;; notes:
 
-;;;; Exceptional-Situations:
+;;;; exceptional-situations:
 
-;;;; Tests:
+;;;; tests:
 #?(pprint-check-bnf nil '(check-bnf))
-:outputs "(CHECK-BNF)"
+:outputs "(check-bnf)"
 
 #?(pprint-check-bnf nil '(check-bnf nil))
-:outputs "(CHECK-BNF ())"
+:outputs "(check-bnf ())"
 
 #?(pprint-check-bnf nil '(check-bnf nil nil))
-:outputs "(CHECK-BNF () ())"
+:outputs "(check-bnf () ())"
 
 #?(pprint-check-bnf nil '(check-bnf nil not-list))
-=> unspecified ; Depending on implementation.
+=> unspecified ; depending on implementation.
 
-;;; [CLISP say](https://clisp.sourceforge.io/impnotes.html#clpp)
+;;; [clisp say](https://clisp.sourceforge.io/impnotes.html#clpp)
 
-;;; > The Lisp Pretty Printer implementation is not perfect yet.
+;;; > the lisp pretty printer implementation is not perfect yet.
 
-;;; So two tests below are ignored in CLISP.
-;;; Fortunately this is just printing, not check-bnf feature itself.
+;;; so two tests below are ignored in clisp.
+;;; fortunately this is just printing, not check-bnf feature itself.
 
 #-clisp
-#?(pprint-check-bnf nil '(check-bnf()
+#?(pprint-check-bnf nil '(check-bnf ()
                            ((a symbol))
                            ((b string))))
-:outputs "(CHECK-BNF ()
-  ((A SYMBOL))
-  ((B STRING)))"
+:outputs "(check-bnf ()
+  ((a symbol))
+  ((b string)))"
 
 #-clisp
-#?(pprint-check-bnf nil '(check-bnf()
+#?(pprint-check-bnf nil '(check-bnf ()
                            ((function-name (or name setf-name))
                             (name symbol)
-                            (setf-name ((eql setf)name)))))
-:outputs "(CHECK-BNF ()
-  ((FUNCTION-NAME (OR NAME SETF-NAME))
-   (NAME SYMBOL)
-   (SETF-NAME ((EQL SETF) NAME))))"
+                            (setf-name ((eql setf) name)))))
+:outputs "(check-bnf ()
+  ((function-name (or name setf-name))
+   (name symbol)
+   (setf-name ((eql setf) name))))"
--- spec/check-bnf.test.asd
+++ spec/check-bnf.test.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 (defsystem "check-bnf.test"
   :version
-  "0.3.3"
+  "0.3.4"
   :depends-on
   (:jingoh "check-bnf")
   :components
